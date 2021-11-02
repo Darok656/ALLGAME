47e28a86fbecc9df13abb56adbeb779579ed16ea
Darok656 <cesar_ortize@hotmail.com>
2021-11-02 14:44:00 -0600

    se agrega carpetas src y vendor

---
+++ src/Format.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Text;
+
+class format extends AnotherClass implements Interface
+{
+    public static function upperText($value)
+    {
+        return Strtoupper($value)
+    }
+}
